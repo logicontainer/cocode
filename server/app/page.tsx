9195bbb4bd70c5cd3a2c21606fa0677254da5047
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2026-03-13 21:19:37 +0100

    Attempt

--- server/app/page.tsx
+++ server/app/page.tsx
@@ -3,13 +3,31 @@ import { useRef } from 'react';
 
 import Editor, { OnMount } from '@monaco-editor/react';
 import type { editor } from 'monaco-editor';
+import { constrainedEditor } from 'constrained-editor-plugin';
+// The initial code loaded into the editor
+const defaultCode = [
+  "// --- READ ONLY ---",
+  "function processData(data) {",
+  "  // Write your logic below:",
+  "  ",
+  "}",
+  "// --- READ ONLY ---"
+].join('\n');
+
 export default function App() {
   const editorRef = useRef<editor.IStandaloneCodeEditor>(null);
 
-  const handleEditorDidMount: OnMount = editor => {
+  const handleEditorDidMount: OnMount = (editor, monaco) => {
     editorRef.current = editor;
-    editor.setValue("// Start value")
-    // editor.updateOptions({ readOnly: true })
+    editor.setValue(defaultCode)
+    const constrainedInstance = constrainedEditor(monaco);
+    const model = editor.getModel();
+    constrainedInstance.initializeIn(editor);
+    restrictions.push({
+      range: [1, 1, 2, 10],
+      allowMultiline: true
+    });
+    constrainedInstance.addRestrictionsTo(model, restrictions);
   }
 
   function showValue() {
