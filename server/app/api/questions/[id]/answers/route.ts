28176c1f2bbd52201df0d439b140135a4ef8fb25
Felix Berg <93908883+felix-berg@users.noreply.github.com>
2026-03-14 01:25:23 +0100

    work (#5)

--- server/app/api/questions/[id]/answers/route.ts
+++ server/app/api/questions/[id]/answers/route.ts
@@ -1,28 +1,31 @@
-import { Answer } from "@/types/api";
+import { Answer, answerNoIdSchema } from "@/types/api";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { parse } from "path";
 
-export async function POST(_: NextRequest, { params }: { params: Promise<{ id: number }> }) {
+export async function POST(req: NextRequest, { params }: { params: Promise<{ id: number }> }) {
   const supabase = createClient(await cookies());
   const { id } = await params
 
+  const parseResult = answerNoIdSchema.safeParse(await req.json())
+  if (!parseResult.success) {
+    return NextResponse.json(parseResult.error, { status: 400 })
+  }
+
+  const answer: Omit<Answer, "id"> = parseResult.data
+
   const { error, data } = await supabase
-    .from("Question")
-    .select("id, Answer ( * )")
-    .eq("id", id)
+    .from("Answer")
+    .insert({
+      question_id: id,
+      text: answer.text
+    })
     
   if (error !== null) {
     console.error("Supabase error", error)
     return NextResponse.json(error, { status: 500 });
   }
 
-  const answers: Answer[] = data
-    .flatMap(ls => ls.Answer)
-    .map(answer => ({ 
-      id: answer.id,
-      text: answer.text ?? ""
-    }))
-
-  return NextResponse.json(answers)
+  return NextResponse.json({})
 }
