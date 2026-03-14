5bc6f755751d7bbd25b221e08ec824f5c7e03ee4
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2026-03-14 01:47:51 +0100

    thingsss

--- types/api.ts
+++ types/api.ts
@@ -11,6 +11,13 @@ export const questionSchema = z.object({
 
 export const questionNoIdSchema = questionSchema.omit({ id: true })
 
+export const answerSchema = z.object({
+  id: idSchema,
+  text: z.string()
+})
+
+export const answerNoIdSchema = answerSchema.omit({ id : true })
+
 export type Session = { id: number ; code: number }
 export type Question = z.infer<typeof questionSchema>
-export type Answer = { id: number ; text: string }
+export type Answer = z.infer<typeof answerSchema>
